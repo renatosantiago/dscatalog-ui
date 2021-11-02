8a75a72f47395a2f14a446fbcca952370edb64bc
Renato Santiago <renatorsanti@gmail.com>
2021-11-02 01:25:47 -0300

    login page authentication

--- src/pages/Auth/components/Login/index.tsx
+++ src/pages/Auth/components/Login/index.tsx
@@ -1,32 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ButtonIcon from 'core/components/ButtonIcon';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import AuthCard from '../Card';
 import './styles.scss';
+import { makeLogin } from 'core/utils/request';
+import { saveSessionData } from 'core/utils/auth';
 
 type FormData = {
-  email: string,
-  password: string
+  username: string;
+  password: string;
 }
 
 const Login = () => {
 
   const { register, handleSubmit } = useForm<FormData>();
-  const onSubmit = (data: FormData) => console.log(data);
+  const [hasError, setHasError] = useState(false);
+  const history = useHistory();
+
+  const onSubmit = (data: FormData) => {
+    makeLogin(data)
+      .then(response => {
+        setHasError(false);
+        saveSessionData(response.data);
+        history.push('/admin');
+      })
+      .catch(() => {
+        setHasError(true);
+      });
+  }
+
 
   return (
     <AuthCard title="login">
+      {hasError && (
+        <div className="alert alert-danger mt-5">
+          Usuário ou senha inválido!
+        </div>
+      )}
       <form className="login-form" onSubmit={handleSubmit(onSubmit)} >
         <input type="email" 
               className="form-control input-base margin-bottom-20" 
               placeholder="Email"
-              {...register('email')} />
+              {...register('username', { required: true })} />
 
         <input type="password" 
               className="form-control input-base" 
               placeholder="Senha"
-              {...register('password')} />
+              {...register('password', { required: true })} />
 
         <Link to="/admin/auth/recover" className="login-link-recover">
           Esqueci a senha
